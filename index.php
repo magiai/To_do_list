3a0b307dda307a1a60e1f721e342148566ed2566
Dominika <adamskdominik@gmail.com>
2017-12-29 16:54:06 +0100

    Html sketch

--- index.php
+++ index.php
@@ -19,3 +19,34 @@ foreach ($items as $item) {
     echo $item['name'], '<br>';
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>To do</title>
+        <!-- czcionka
+        <link href="" rel="stylesheet">
+        -->
+        <link rel="stylesheet" href="css/main.css">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="list">
+            <h1 class="header">to do</h1>
+
+            <ul>
+                <li><span class="item">Pick up shopping</span></li>
+                <li><span class="item done">Learn php</span></li>
+            </ul>
+
+            <form class="item-add" action="add.php" method="post">
+                <input type="text" name="name" placeholder="What you gonna do." class="input" autocomplete="off" required>
+                <input type="submit" value="Add" class="submit">
+            </form>
+        </div>
+
+
+    </body>
+</html>
